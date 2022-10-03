3ef28411a2c47fda85416184fe55b73340545edd
HeedfulMoss <dayerbe@icloud.com>
2022-10-02 22:01:23 -0400

    new homepage

---
+++ Homepage.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+import numpy as np
+
+st.set_page_config(page_title='Ichimoku Cloud Homepage')
+st.header('Please select a ticker')
+
+st.sidebar.subheader('Ticker Selector')
+ticker_list = pd.read_csv('SP500 Index.csv')
+tickerSymbol = st.sidebar.selectbox('Stock ticker', ticker_list)
+tickerData = yf.Ticker(tickerSymbol) 
+tickerDf = tickerData.info()
+# Ticker data
+st.header('**Ticker data**')
+st.write(tickerDf)
